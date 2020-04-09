a24438f9283188c2ac9699e4af887f09c3fa179c
Robin Davoust <davoust88@hotmail.com>
2020-04-09 07:47:53 +0200

    footer and newsletter

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    FooterComponent,
+    NewsletterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
