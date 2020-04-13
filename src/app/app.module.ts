17fefedb6bd6f1a0ab9b8df65c99f18682266995
Robin Davoust <davoust88@hotmail.com>
2020-04-13 14:37:21 +0200

    footerNewsletter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from './footer/footer.component';
 import { NewsletterComponent } from './newsletter/newsletter.component';
+import { DogshopComponent } from './dogshop/dogshop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     FooterComponent,
-    NewsletterComponent
+    NewsletterComponent,
+    DogshopComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dogshop/dogshop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dogshop',
+  templateUrl: './dogshop.component.html',
+  styleUrls: ['./dogshop.component.css']
+})
+export class DogshopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
