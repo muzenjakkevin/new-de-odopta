b53586666f0da37c8604fac8954c0cfbb54ad2ec
muzenjakkevin <muzenjak57@gmail.com>
2020-04-19 23:17:05 +0200

    final final changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from '../_helpers';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { AboutComponent } from "./about/about.component";
